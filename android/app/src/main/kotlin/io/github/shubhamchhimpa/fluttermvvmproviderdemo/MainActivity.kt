26473876f43fc5dfc204ba9e24896933458a5dad
shubhamchhimpa <chhimpa.shubh04@gmail.com>
2020-07-11 12:39:53 +0530

    initial code commit

---
+++ android/app/src/main/kotlin/io/github/shubhamchhimpa/fluttermvvmproviderdemo/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.shubhamchhimpa.fluttermvvmproviderdemo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MVVM + Provider Demo"),
+      ),
+      body: Center(
+        child: Text("Initial Code"),
+      ),
+    );
+  }
+}
